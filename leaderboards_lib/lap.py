9464148c056246f8be82fd27b1238882d29046da
Ken <dragonkenkj@gmail.com>
2024-03-12 16:52:54 -0700

    Added third party libraries including firebase-admin and numpy

--- leaderboards_lib/lap.py
+++ leaderboards_lib/lap.py
@@ -7,7 +7,7 @@ import ac
 
 IP_ADDRESS = "10.0.0.153"
 
-FIELDS = ["distance_offset", "time_elapsed"]
+FIELDS = ["distance_offset", "time_elapsed", "gas", "brake"]
 
 class Lap:
     def __init__(self, track):
@@ -16,11 +16,15 @@ class Lap:
         self.lap_time = None
         self.sector_times = None
         self.distance_time = []
+        self.last_offset = 0
 
-    def add(self, offset, elapsed):
-        self.distance_time.append([float(offset), int(elapsed)])
+    def add(self, offset, elapsed, gas, brake):
+        if offset > self.last_offset:
+            self.distance_time.append([float(offset), float(elapsed/1000), float(gas), float(brake)])
+            self.last_offset = offset
 
     def upload(self, cur_user_id, is_best_lap):
+        self.distance_time.append([1, self.lap_time/1000, 1.0, 0.0])
         timestamp = math.floor(time.time() * 1000)
         data = urllib.parse.urlencode({"lap_time": self.lap_time, "track": self.track, "invalidated": self.invalidated, "sector_times": self.sector_times, "timestamp": timestamp, "is_best_lap": is_best_lap}).encode("ascii")
         req = urllib.request.Request(url='http://{}:8000/upload-lap/'.format(IP_ADDRESS), data=data)
--- leaderboards_lib/leaderboards_lib.py
+++ leaderboards_lib/leaderboards_lib.py
@@ -1,3 +1,4 @@
+# AC runs on Python 3.3.5
 import urllib.request
 import json
 import os
@@ -13,16 +14,22 @@ from sim_info import info
 from leaderboards_lib import lap
 from leaderboards_lib import leaderboards_ui
 
+# Third party libs
+import numpy as np
+import firebase_admin # 2.1.1
+
 IP_ADDRESS = "10.0.0.153"
 TRACK_NAMES = {"bahrain_2020": "Bahrain International Circuit", "jeddah21": "Jeddah Corniche Circuit", "rt_suzuka": "Suzuka Circuit", "actk_losail_circuit": "Losail International Circuit", "acu_cota_2021": "Circuit of the Americas", "acu_mexico_2021": "Autódromo Hermanos Rodríguez", "melbourne22": "Albert Park Circuit"}
+# Default time if user hasn't set valid time on the selected track
 DEFAULT_TIME = 999999999
-# Default time is user hasn't set valid time on the selected track
 
 class Leaderboards:
     def __init__(self):
         app_window = ac.newApp("Performance Delta")
         self.track = TRACK_NAMES[ac.getTrackName(0)]
         self.lap = lap.Lap(self.track)
+        self.first_lap = True
+        self.last_time = 0 # Used top keep track of new lap
         self.lap_count = 0
         self.cur_username, self.cur_user_id = self.init_server()
         self.best_lap_offset, self.best_lap_elapsed, self.best_lap_time = self.get_best_lap()
@@ -50,13 +57,12 @@ class Leaderboards:
             response = json.loads(response.read().decode(response.info().get_param('charset') or 'utf-8'))
             best_lap_time = response['lapTime']
             best_lap_id = response['id']
-        best_lap_dir = 'best_laps\{}\{}'.format(self.cur_user_id, self.track)
-        if not os.path.isfile('{}\{}.csv'.format(best_lap_dir, best_lap_id)) or best_lap_time == 0:
+        best_lap_f = 'best_laps\{}\{}\{}.csv'.format(self.cur_user_id, self.track, best_lap_id)
+        if not os.path.isfile(best_lap_f) or best_lap_time == 0:
             return None, None, DEFAULT_TIME
-        with open('best_laps\{}\{}\{}.csv'.format(self.cur_user_id, self.track, best_lap_id), 'rb') as csvfile:
-            reader = csv.reader(csvfile)
-            data = list(reader)
-        offset, elapsed = data[:,0], data[:,1]
+        data = np.loadtxt(best_lap_f, delimiter=',', skiprows=1)
+        offset, elapsed = data[:,0].tolist(), data[:,1].tolist()
+        elapsed = [x * 1000 for x in elapsed]
         return offset, elapsed, best_lap_time
 
     def update_delta(self, offset, elapsed):
@@ -71,34 +77,38 @@ class Leaderboards:
         return "Leaderboards"
 
     def acUpdate(self, deltaT):
-        cur_lap_count = ac.getCarState(0, acsys.CS.LapCount)
-        if cur_lap_count > self.lap_count:
-            self.lap_count = cur_lap_count
-            last_lap_time = ac.getCarState(0, acsys.CS.LastLap)
-            last_sector_times = ac.getLastSplits(0)
-            self.lap.lap_time = last_lap_time
-            self.lap.sector_times = last_sector_times
-            is_best_lap = not self.lap.invalidated and self.lap.lap_time < self.best_lap_time
-            if is_best_lap:
-                self.best_lap_time = self.lap.lap_time
-                distance_time = list(zip(*self.lap.distance_time))
-                self.best_lap_offset, self.best_lap_elapsed = list(distance_time[0]), list(distance_time[1])
-                self.ui.update_best_lap_time(self.best_lap_time)
-            self.lap.upload(self.cur_user_id, is_best_lap)
+        cur_time = ac.getCarState(0, acsys.CS.LapTime)
+        # Check if new lap
+        if cur_time < self.last_time:
+            if not self.first_lap:
+                last_lap_time = ac.getCarState(0, acsys.CS.LastLap)
+                last_sector_times = ac.getLastSplits(0)
+                self.lap.lap_time = last_lap_time
+                self.lap.sector_times = last_sector_times
+                is_best_lap = not self.lap.invalidated and self.lap.lap_time < self.best_lap_time
+                if is_best_lap:
+                    self.best_lap_time = self.lap.lap_time
+                    distance_time = list(zip(*self.lap.distance_time))
+                    self.best_lap_offset, self.best_lap_elapsed = list(distance_time[0]), list(distance_time[1])
+                    self.ui.update_best_lap_time(self.best_lap_time)
+                self.lap.upload(self.cur_user_id, is_best_lap)
+            self.lap_count = ac.getCarState(0, acsys.CS.LapCount)
             self.lap = lap.Lap(self.track)
             self.ui.update_invalidated(False)
             self.ui.update_lap_counter(self.lap_count)
+            self.first_lap = False
         if self.check_invalidated():
             self.lap.invalidated = True
             self.ui.update_invalidated(True)
         offset = ac.getCarState(0, acsys.CS.NormalizedSplinePosition)
         elapsed = ac.getCarState(0, acsys.CS.LapTime)
+        gas =  ac.getCarState(0, acsys.CS.Gas)
+        brake =  ac.getCarState(0, acsys.CS.Brake)
         if self.best_lap_time != DEFAULT_TIME:
             self.update_delta(offset, elapsed)
-        # ac.log(type(offset))
-        # ac.log(offset)
-        self.lap.add(offset, elapsed)
+        self.lap.add(offset, elapsed, gas, brake)
         self.ui.update_lap_time(ac.getCarState(0, acsys.CS.LapTime))
+        self.last_time = cur_time
 
     def acShutdown(self):
         req = urllib.request.Request(url='http://{}:8000/app-shutdown/'.format(IP_ADDRESS))
